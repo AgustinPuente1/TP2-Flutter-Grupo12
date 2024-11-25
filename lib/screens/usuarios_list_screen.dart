c4df0f1016e8166bb723a253eae62b27373f6faa
unknown <agustinpuente90@gmail.com>
2024-11-25 02:58:01 -0300

    Modificacion mock y List con nuevas tarjetas

--- lib/screens/usuarios_list_screen.dart
+++ lib/screens/usuarios_list_screen.dart
@@ -2,7 +2,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:tp2_flutter_grupo12/widgets/usuarios_card.dart';
 import 'package:tp2_flutter_grupo12/widgets/usuarios_search_bar.dart';
-import 'package:tp2_flutter_grupo12/mocks/people_mock.dart' show elements;
+import 'package:tp2_flutter_grupo12/mocks/usuarios_mock.dart' show elements;
 
 class UsuariosListScreen extends StatefulWidget {
   const UsuariosListScreen({super.key});
@@ -34,80 +34,73 @@ class _UsuariosListScreenState extends State<UsuariosListScreen> {
   }
 
   void _updateSearch(String? query) {
-    setState(() {
-      _searchQuery = query ?? '';
-      if (_searchQuery.isEmpty) {
-        _auxiliarElements = elements; // Restablecer al estado original
-      } else {
-        _auxiliarElements = elements.where((element) {
-          return element[1].toLowerCase().contains(_searchQuery.toLowerCase());
-        }).toList();
+  setState(() {
+    _searchQuery = query ?? '';
+    if (_searchQuery.isEmpty) {
+      _auxiliarElements = elements; // Restablecer al estado original
+    } else {
+      _auxiliarElements = elements.where((element) {
+        final fullName = '${element[2]} ${element[3]}'.toLowerCase();
+        return fullName.contains(_searchQuery.toLowerCase());
+      }).toList();
       }
     });
   }
 
+
   @override
-Widget build(BuildContext context) {
-  return SafeArea(
-    top: true,
-    child: Scaffold(
-      body: Column(
-        children: [
-          UsuariosSearchBar(
-            isActive: _searchActive,
-            searchController: _searchController,
-            focusNode: _focusNode,
-            onClose: () {
-              _searchController.clear();
-              FocusManager.instance.primaryFocus?.unfocus();
-              _updateSearch('');
-            },
-            onBack: () {
-              setState(() {
-                _searchActive = !_searchActive;
-              });
-            },
-            onSearch: (value) => _updateSearch(value),
-          ),
-          listItemsArea(),
-        ],
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: true,
+      child: Scaffold(
+        body: Column(
+          children: [
+            UsuariosSearchBar(
+              isActive: _searchActive,
+              searchController: _searchController,
+              focusNode: _focusNode,
+              onClose: () {
+                _searchController.clear();
+                FocusManager.instance.primaryFocus?.unfocus();
+                _updateSearch('');
+              },
+              onBack: () {
+                setState(() {
+                  _searchActive = !_searchActive;
+                });
+              },
+              onSearch: (value) => _updateSearch(value),
+            ),
+            listItemsArea(),
+          ],
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 
   Expanded listItemsArea() {
-  return Expanded(
-    child: ListView.builder(
-      physics: const BouncingScrollPhysics(),
-      itemCount: _auxiliarElements.length,
-      itemBuilder: (BuildContext context, int index) {
-        final element = _auxiliarElements[index];
+    return Expanded(
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        itemCount: _auxiliarElements.length,
+        itemBuilder: (BuildContext context, int index) {
+          final element = _auxiliarElements[index];
 
-        return UsuariosCard(
-          avatarPath: element[0],
-          name: element[1],
-          role: element[2],
-          isFavorite: element[4],
-          stars: element[3],
-          onTap: () {
-            Navigator.pushNamed(
-              context,
-              'custom_list_item',
-              arguments: {
-                'avatar': element[0],
-                'name': element[1],
-                'cargo': element[2],
-                'stars': element[3],
-                'favorite': element[4],
-              },
-            );
-          },
-        );
-      },
-    ),
-  );
-}
-
-  
+          return UsuariosCard(
+            avatar: element[1],
+            firstName: element[2],
+            lastName: element[3],
+            gender: element[5],
+            country: element[6],
+            isFavorite: element[7],
+            onFavoriteTap: () {
+              setState(() {
+                element[7] = !element[7];
+              });
+            },
+          );
+        },
+      ),
+    );
+  }
 }
--- lib/widgets/usuarios_card.dart
+++ lib/widgets/usuarios_card.dart
@@ -1,76 +1,89 @@
 import 'package:flutter/material.dart';
 
 class UsuariosCard extends StatelessWidget {
-  final String avatarPath;
-  final String name;
-  final String role;
+  final String avatar;
+  final String firstName;
+  final String lastName;
+  final String gender;
+  final String country;
   final bool isFavorite;
-  final int stars;
-  final VoidCallback? onTap;
+  final VoidCallback? onFavoriteTap;
 
   const UsuariosCard({
     Key? key,
-    required this.avatarPath,
-    required this.name,
-    required this.role,
+    required this.avatar,
+    required this.firstName,
+    required this.lastName,
+    required this.gender,
+    required this.country,
     required this.isFavorite,
-    required this.stars,
-    this.onTap,
+    this.onFavoriteTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        height: 100,
-        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: const [
-            BoxShadow(
-              color: Color.fromARGB(31, 206, 219, 246),
-              blurRadius: 3,
-              offset: Offset(0, 3),
-            )
-          ],
-        ),
-        child: Row(
-          children: [
-            // Imagen del avatar
-            Image.asset(
-              'assets/avatars/$avatarPath.png',
-              width: 50,
-              height: 50,
-            ),
-            const SizedBox(width: 10),
-            // Información del usuario
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    name,
-                    style: const TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                    ),
+    return Container(
+      height: 100,
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromARGB(31, 206, 219, 246),
+            blurRadius: 3,
+            offset: Offset(0, 3),
+          )
+        ],
+      ),
+      child: Row(
+        children: [
+          // Imagen del avatar
+          Image.asset(
+            'assets/avatars/$avatar.png',
+            width: 50,
+            height: 50,
+          ),
+          const SizedBox(width: 10),
+          // Nombre y apellido
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  firstName,
+                  style: const TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Text(role),
-                ],
-              ),
+                ),
+                Text(lastName),
+              ],
             ),
-            // Icono y estrellas
-            Icon(
+          ),
+          // Icono de género
+          Icon(
+            gender == "Male" ? Icons.male : Icons.female,
+            color: gender == "Male" ? Colors.blue : Colors.pink,
+          ),
+          const SizedBox(width: 10),
+          // Bandera del país
+          Image.asset(
+            'assets/flags/$country.png',
+            width: 30,
+            height: 30,
+          ),
+          const SizedBox(width: 10),
+          // Estrella para favoritos
+          IconButton(
+            icon: Icon(
               isFavorite ? Icons.star : Icons.star_border_outlined,
               color: Colors.yellow[700],
             ),
-            const SizedBox(width: 5),
-            Text('$stars'),
-          ],
-        ),
+            onPressed: onFavoriteTap,
+          ),
+        ],
       ),
     );
   }
